1c024efa23287342d9a2433f0c2176c2b1e04239
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-05-05 07:28:52 -0700

    nibble added

--- bittle.java
+++ bittle.java
@@ -13,64 +13,69 @@ class Main {
 		//other initializations
 		ArrayList<Integer> bit_guess_count = new ArrayList<Integer>() ;
 		ArrayList<Integer> byte_guess_count = new ArrayList<Integer>() ;
+		ArrayList<Integer> nibble_guess_count = new ArrayList<Integer>() ;
 
 		//and, now to the game
 		boolean is_play_over = false ; // for the "play"
 		boolean is_game_over = false ; // for each specific sub-game of bittle
-      boolean isPrintingInstructions = true ;
-		while(!is_play_over) {
+		boolean isPrintingInstructions = true ;
+			while(!is_play_over) {
+					
+				is_game_over = false ;
+
+				// player action...
+				System.out.print(".................................... \n" + 
+									"Would you like to:\n" + 
+									"  p - play bit mode (novice)?\n" +
+									"  n - play nibble mode (medium)?\n" +
+									"  a - play byte mode (advanced)?\n" +
+									"  i - turn on/off instructions?\n" + 
+									"  s - display your stats?\n" + 
+									"  q - quit?\n" + 
+									"\n>> ") ;
+				String play = s.nextLine() ;
+				System.out.print(".................................... \n") ; 
+
+				if (play.equals("q")) { // QUITTING
+					is_play_over = true ;
+					s.close() ;
+					System.out.println("You have sucessfully quit bittle :)\n" + 
+										"Have a \"binary\"-ific day!\n") ;
+				} 
+				else if (play.equals("s")) { // PRINT PLAYER STATS
+					System.out.println("\n---Bit Stats--- \n" + statsPrinter(bit_guess_count)) ;
+					System.out.println("\n---Nibble Stats--- \n" + statsPrinter(nibble_guess_count)) ;
+					System.out.println("\n---Byte Stats--- \n" + statsPrinter(byte_guess_count)) ;
+				} 
+			else if (play.equals("i")) { // INSTRUCTIONS on HOW TO PLAY
+				isPrintingInstructions = !isPrintingInstructions ;
+				System.out.println(" -- \"how to play\" switched " +
+								(isPrintingInstructions ? "on" : "off")) ;
+			}
+			else if (play.equals("p") // PLAY GAME, bit-mode (len = 1) 
+					|| play.equals("n") // nibble mode (len = 4)
+					|| play.equals("a")) { // advanced / byte mode (len = 8)
 				
-			is_game_over = false ;
-
-			// player action...
-			System.out.print(".................................... \n" + 
-								"Would you like to:\n" + 
-								"  p - play bit mode (novice)?\n" +
-								"  a - play byte mode (advanced)?\n" +
-                        "  i - turn on/off instructions?\n" + 
-								"  s - display your stats?\n" + 
-								"  q - quit?\n" + 
-								"\n>> ") ;
-			String play = s.nextLine() ;
-			System.out.print(".................................... \n") ; 
-
-			if (play.equals("q")) { // QUITTING
-				is_play_over = true ;
-				s.close() ;
-				System.out.println("You have sucessfully quit bittle :)\n" + 
-									"Have a \"binary\"-ific day!\n") ;
-			} 
-			else if (play.equals("s")) { // PRINT PLAYER STATS
-				System.out.println("\n---Bit Stats--- \n" + statsPrinter(bit_guess_count)) ;
-				System.out.println("\n---Byte Stats--- \n" + statsPrinter(byte_guess_count)) ;
-			} 
-         else if (play.equals("i")) { // INSTRUCTIONS on HOW TO PLAY
-            isPrintingInstructions = !isPrintingInstructions ;
-            System.out.println(" -- \"how to play\" switched " +
-                              (isPrintingInstructions ? "on" : "off")) ;
-         }
-			else if (play.equals("p") || play.equals("a")) { // PLAY GAME
-				// P = regular (bit) mode
-				// A = byte mode
 				System.out.println("Starting game now.") ;
 			
-			// switching to/fro   
+				// switching to/fro   
 				int len ;
-            // len = 1 if entered letter was "p", and len = 8 otherwise
-            len = play.equals("p") ? 1 : 8 ;
-				
+				// len = 1 if entered letter was "p", and len = 8 otherwise
+				len = play.equals("p") ? 1 : 8 ;
+				//then switch len to 4 if it's in nibble mode
+				len = play.equals("n") ? 4 : len ;
 
-			   //now, randomly creating the answer
+				//now, randomly creating the answer
 				String ans = "" ;
 				for (int i = 0; i < len; i++) {
-               //the value can either be 0 or 1; randomly chosen - 
-               ans += ((int)(Math.random() * 2) == 0) ? "0" : "1" ;
+				//the value can either be 0 or 1; randomly chosen - 
+				ans += ((int)(Math.random() * 2) == 0) ? "0" : "1" ;
 				}
 
-            if (isPrintingInstructions) {
-               // instructions on how to play
-               System.out.println(instructionsToPlay(len)) ;
-            }
+				if (isPrintingInstructions) {
+					// instructions on how to play
+					System.out.println(instructionsToPlay(len)) ;
+				}
 
 				// starting the game
 				ArrayList<String> past_guesses = new ArrayList<String>() ;
@@ -107,30 +112,32 @@ class Main {
 							incorrect = true ;
 						}
 					}
-			   
+				
 					//printing
 					past_guesses.add(printing) ;
 					printPastGuesses(past_guesses);
 
-               // conditions for exiting the game
-               // and, I use 42 as a max for obvious reasons here.
-               // if you don't understand, then, so long.
-               if (!incorrect || past_guesses.size() >= 42) {
-                  if (len == 1)  //bit mode
-                     bit_guess_count.add(guess_count) ;
-                  else           //byte mode
-                     byte_guess_count.add(guess_count) ;
+					// conditions for exiting the game
+					// and, I use 42 as a max for obvious reasons here.
+					// if you don't understand, then, so long.
+					if (!incorrect || past_guesses.size() >= 42) {
+						if (len == 1)  		//bit mode
+							bit_guess_count.add(guess_count) ;
+						else if (len == 4) 	// nibble mode
+							nibble_guess_count.add(guess_count) ;
+						else           		//byte mode
+							byte_guess_count.add(guess_count) ;
 
-                  is_game_over = true ;
-                  if (!incorrect) 
-                     youWinOnceAgain(guess_count, len, ans, "win");
-                  else if (past_guesses.size() >= 42) {
-                     youWinOnceAgain(guess_count, len, ans, "lose"); 
-                  }
-               }
+						is_game_over = true ;
+						if (!incorrect) 
+							youWinOnceAgain(guess_count, len, ans, "win");
+						else if (past_guesses.size() >= 42) {
+							youWinOnceAgain(guess_count, len, ans, "lose"); 
+						}
+					}
 				}    
 			} 
-			else { //bc sometimes sentient beings aren't smart
+			else { //bc sometimes sentient beings aren't the most brilliant
 				System.out.println("please have some sense and input one of the below options.");
 			}
 		}  
@@ -156,31 +163,33 @@ class Main {
 			if (g > max)
 				max = g ;
 		}
-	  if (guess_history.size() != 0)
-		  maths /= guess_history.size() ;
-		// some random stuff
-		String ret = "" ;
-		if (maths >= 10) {
-			ret = "\nYour odds of being a robot are " + (maths * 30 / 100) ;
-		}
-	  if (min == -1 || max == -1) {
-		 return "You haven't tried this mode yet :( " ;
-	  } //else {
-		return   " games played : " + guess_history.size() + 
-               "\n average guess count : " + Double.toString(maths) + 
-		 		   "\n minimum guess count : " + min +
-		 		   "\n maximum guess count : " + max +
-		         ret ;
-	  //}
+		if (guess_history.size() != 0)
+			maths /= guess_history.size() ;
+			// some random stuff
+			String ret = "" ;
+			if (maths >= 10) {
+				ret = "\nYour odds of being a robot are " + (maths * 30 / 100) ;
+			}
+		// and, a lengthy return statement, starring my new obsession with ?:
+		return (guess_history.size() == 0) ? 
+				// if the mode hasn't been played yet
+				"You haven't tried this mode yet :( " 
+				: 
+				// if mode has been played and there is data to show
+				" games played : " + guess_history.size() + 
+				"\n average guess count : " + Double.toString(maths) + 
+				"\n minimum guess count : " + min +
+				"\n maximum guess count : " + max +
+				ret ;
+	  
 	}
-	// I know it's spelled 'toggle', but the extra e makes it exciting
 	public static String instructionsToPlay(int binaryLength) {
 		return (".................................... \n" +
 		"How to play:\n" +
-		"A \"bit\" is a one or zero. bits (and bytes) represent stuff in binary.\n" +
+		"A \"bit\" is a one or zero. bits (and nibbles, and bytes) represent stuff in binary.\n" +
 		"Eight bits make a byte!\n" +
 		"enter a starting " + konstantFinder(binaryLength) + ", then repeatedly enter more " +
-      konstantFinder(binaryLength) + "s until you guess the correct " + konstantFinder(binaryLength) + " \n" +
+        konstantFinder(binaryLength) + "s until you guess the correct " + konstantFinder(binaryLength) + " \n" +
 		" ->  * * means it's not in the " + konstantFinder(binaryLength) + "\n" +
 		" ->  + + means it's in " + konstantFinder(binaryLength) + ", incorrect spot\n" +
 		" ->      (by itself) means it's in " + konstantFinder(binaryLength) + ", correct spot\n" +
@@ -215,13 +224,15 @@ class Main {
 							"The " + konstantFinder(len) + " was: " + ans + 
 							"\n*******************************\n");    
 	}
-	public static String konstantFinder(int length) {
-		String konstant ;
-		if (length == 1) 
-			konstant = "bit" ;
-		else 
-			konstant = "byte" ;
-		return konstant ;
+	// konstantFinder is for easy translation between answer's length to Strings
+	public static String konstantFinder(int len) {
+		// if the length = 1, then choose "bit"
+		// otherwise choose "byte"
+		// if the length = 4, then choose "nibble", else leave as is
+		// return chosen string
+		String ret = (len == 1) ? "bit" : "byte" ;
+		ret = (len == 4) ? "nibble" : ret ;
+		return ret ;
 	}
 	
 }
